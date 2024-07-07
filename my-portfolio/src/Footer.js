13912af02eb7bb3ddd7321d5a40c5e4f7f26b786
seaseeraw <seaseeraw12@gmail.com>
2024-07-07 13:20:41 +1000

    Updates

--- my-portfolio/src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Footer.css';
-const Footer = () => (
-  <footer className="footer">
-    <p>© {new Date().getFullYear()} Your Name. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
